6988a5aa7966ff928bb78b4c8aa973a4d437a3d8
Leo <m.leo.cen@outlook.com>
2022-07-24 01:38:23 +0800

    test: 4.9 verify blog posts 'id' property & change schema toJSON() to make test passes

--- tests/api.test.js
+++ tests/api.test.js
@@ -9,3 +9,8 @@ test('GET /api/blogs return json', () => {
     .expect(200)
     .expect('Content-Type', /application\/json/)
 })
+
+test('unique identifier property of the blog posts is named id', async () => {
+  const firstBlog = await api.get('/api/blogs')
+  expect(firstBlog.body[0].id).toBeDefined()
+})
