89560d465ba1235bda6aa709db4a2210f6a7094a
Leo <m.leo.cen@outlook.com>
2022-08-10 02:50:30 +0800

    feat: 4.18

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 
 const app = express()
 
@@ -9,5 +10,6 @@ app.use(cors())
 app.use(express.json())
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 module.exports = app
